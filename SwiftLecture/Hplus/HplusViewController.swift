86c99477e35135b6be1abfa7b375b1758f069837
Fang Huai An <liddle.fang@gmail.com>
2020-06-02 16:11:20 +0800

    json

--- SwiftLecture/Hplus/HplusViewController.swift
+++ SwiftLecture/Hplus/HplusViewController.swift
@@ -27,14 +27,35 @@ class HplusViewController: UIViewController {
         switch( response.result ){
         case let .success(value) :
         
+            //print(value)
+            //print(response.response!.statusCode)
+            
+            if let dataFromString = value.data(using: .utf8, allowLossyConversion: false) {
+                let json = try! JSON(data: dataFromString)
+                
+                //print(json)
+                
+                for (index,subJson):(String, JSON) in json {
+                    // Do something you want
+                    
+                    
+                    
+                    
+                    let name : String = subJson["name"].stringValue
+                    let metabolicRate : Double = subJson["metabolicRate"].doubleValue
+                    
+                    print("\( name ): \( metabolicRate )")
+                    
+                }
+                
+                
+            }
+            
+            
             // 注意 guard 的使用方法。
             guard 200 == response.response!.statusCode else {
                 
-                if let dataFromString = value.data(using: .utf8, allowLossyConversion: false) {
-                    let json = try! JSON(data: dataFromString)
-                    
-                    print(json)
-                }
+                
                 
                 return
             }
